fc29dc72a382639f1b10636ddfef7b1b0bc32487
Ethan Chan <metakirby5@gmail.com>
2015-02-01 02:53:51 -0800

    rektrektrektrektrekt

--- js/kavinisms.js
+++ js/kavinisms.js
@@ -2,6 +2,7 @@ KAVINISMS = [
   "You need to <b>calm</b> down.",
   "How dare you.",
   "Rekt.",
+  "Rektrektrektrektrekt.",
   "You should be ashamed of yourself.",
   "Wow.",
   "You <i>son</i> of a bitch.",
